c61e9cc545b314fa58b96a3832451eb36d569d5a
Bob Starr <bobstarr@rtzaudio.com>
2020-09-26 18:01:00 -0400

    Update DisplayTask.c

--- DisplayTask.c
+++ DisplayTask.c
@@ -199,10 +199,10 @@ void DrawUV(void)
     uint32_t spacing;
     tRectangle rect;
     static char buf[128];
-    float v;
+    //float v;
     float level;
     float power;
-    float step;
+    //float step;
     float fullscale;
     float percentage;
 
@@ -264,7 +264,7 @@ void DrawUV(void)
 
             /* The ADC vref is 4.096V */
             fullscale = (g_sys.adcID == AD7798_ID) ?  (float)AD7798_FULLSCALE : (float)AD7799_FULLSCALE;
-
+#if 0
             /* Calculate the voltage per ADC step */
             step = ADC_VREF / fullscale;
 
@@ -273,9 +273,11 @@ void DrawUV(void)
 
             /* sensor Vout = 0.71 x UV-C power in mW/cm2 */
             power = v / 0.71f;
+#endif
+            power = level / 6323.07f;
 
-            //sprintf(buf, "%d: %.3f", i+1, power);
-            sprintf(buf, "%d: %6x", i, g_sys.adcLevel[i]);
+            sprintf(buf, "%d: %.2f", i+1, power);
+            //sprintf(buf, "%d: %6x", i, g_sys.adcLevel[i]);
 
             //float percentage = (power / 7.0f) * 100.0f;
             percentage = (level / fullscale) * 100.0f;
